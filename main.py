247c8024734ccd9f02b1ff5390d0bf72938716ac
Drake Samuel <817144@stma.k12.mn.us>
2021-04-01 10:24:52 -0500

    test

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 def close10(x, y):
-    abs(x)
-    abs(y)
-    if 10 - x < 10 - 7:
-        return x
-    elif 10 - x > 10 - y:
+ 
+    if abs(10 - x) < abs(10 - y):
         return y
-    elif 10 - x + 10 - y:
+    elif abs(10 - x) > abs(10 - y):
+        return x
+    elif abs(10 - x) == abs(10 - y):
         return 0
 
-print(close10(12, 88))
+print(close10(11, 9))
